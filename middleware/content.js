83459cd920d65b326573f190318ec306203db32f
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-05-10 14:02:13 +0530

    Akshay | bug-745 | fixed content delivery

---
+++ middleware/content.js
@@ -0,0 +1,26 @@
+const userData = [
+  {
+    id: 1,
+    name: "Akshay",
+    age: 45,
+  },
+  {
+    id: 2,
+    name: "Yash",
+    age: 35,
+  },
+  {
+    id: 3,
+    name: "Raj",
+    age: 25,
+  },
+  {
+    id: 4,
+    name: "Rohan",
+    age: 15,
+  },
+];
+
+module.exports.fetchUsers = (req, res, next) => {
+  res.send("data");
+};

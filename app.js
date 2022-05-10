7b4710d0eba0750df899f36c81f674c1296c3e86
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-05-10 13:56:28 +0530

    Akshay | Feature-124 | Popup window

---
+++ app.js
@@ -0,0 +1,14 @@
+const User = require("./middleware/content.js");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+let app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3000, () => console.log("Server is running on port 3000"));
+
+app.get("/url", User.fetchUsers);

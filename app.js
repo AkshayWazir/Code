2ccdda867e0438e08bc325638e3ed8747e98400b
akshay.rein-at-908805445373 <waizaeer4@gmail.com>
2022-05-18 12:17:10 +0530

    test

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(4000, () => console.log("Server is running on port 4000"));
 
 app.get("/url/n", User.fetchUsers);
